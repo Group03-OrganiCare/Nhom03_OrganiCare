60a5174204bdc1f140fe55d92aa41798453b551a
Băng Châu <chauchb19411@st.uel.edu.vn>
2021-10-30 15:11:52 +0700

    2 màn hình Payment Còn phần bo tròn nút thì không chính xác lắm, mặc dù hiện được trên giao diện Chưa xử lý sự kiện nào hết

---
+++ app/src/main/java/com/example/nhom03_organicare/PaymentOptions.java
@@ -0,0 +1,42 @@
+package com.example.nhom03_organicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.adapter.PaymentAdapter;
+import com.example.model.Payment;
+
+import java.util.ArrayList;
+
+public class PaymentOptions extends AppCompatActivity {
+
+    ListView lvPaymentOptions;
+    ArrayList<Payment> payments;
+    PaymentAdapter paymentAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_options);
+        linkViews();
+        initData();
+        loadData();
+    }
+
+    private void linkViews() {
+        lvPaymentOptions = findViewById(R.id.lvPaymentOptions);
+    }
+
+    private void initData() {
+        payments = new ArrayList<Payment>();
+        payments.add(new Payment(R.drawable.zalopay,"Thanh toán bằng ZaloPay"));
+        payments.add(new Payment(R.drawable.momo,"Thanh toán bằng MoMo"));
+        payments.add(new Payment(R.drawable.airpay,"Thanh toán bằng Airpay"));
+    }
+
+    private void loadData() {
+        paymentAdapter = new PaymentAdapter(this,R.layout.item_payment,payments);
+        lvPaymentOptions.setAdapter(paymentAdapter);
+    }
+}
