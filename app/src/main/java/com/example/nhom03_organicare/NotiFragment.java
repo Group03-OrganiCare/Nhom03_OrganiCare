0743759dbbdefa711b8862b40e3056a43f2b189e
thangnh19411 <thangnh19411@st.uel.edu.vn>
2021-11-01 10:12:29 +0700

    Notification

--- app/src/main/java/com/example/nhom03_organicare/NotiFragment.java
+++ app/src/main/java/com/example/nhom03_organicare/NotiFragment.java
@@ -7,15 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.model.Category;
+
+import java.util.ArrayList;
 
 
 public class NotiFragment extends Fragment {
 
+    ListView lvNotice;
+    ArrayList<String> arrayNotice;
+    ArrayAdapter arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_noti, container, false);
+        View view = inflater.inflate(R.layout.fragment_noti, container, false);
+        //Link view
+        lvNotice = view.findViewById(R.id.lvNotice);
+        arrayNotice = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayNotice );
+        lvNotice.setAdapter(arrayAdapter);
+        return view;
     }
+
+    private ArrayList<String> initData(){
+        arrayNotice.add("Notice 1");
+        arrayNotice.add("Notice 2");
+        arrayNotice.add("Notice 3");
+        arrayNotice.add("Notice 4");
+        arrayNotice.add("Notice 5");
+        arrayNotice.add("Notice 6");
+        arrayNotice.add("Notice 7");
+        arrayNotice.add("Notice 8");
+        arrayNotice.add("Notice 9");
+        arrayNotice.add("Notice 10");
+        arrayNotice.add("Notice 11");
+        arrayNotice.add("Notice 12");
+        arrayNotice.add("Notice 13");
+        arrayNotice.add("Notice 14");
+        arrayNotice.add("Notice 15");
+        arrayNotice.add("Notice 16");
+        return arrayNotice;
+    }
+
 }
