2f87af403a4a6f6cfcd7918839d8d862097a3fab
Băng Châu <chauchb19411@st.uel.edu.vn>
2021-11-28 22:17:27 +0700

    CardInfo

---
+++ app/src/main/java/com/example/nhom03_organicare/CardInfo.java
@@ -0,0 +1,14 @@
+package com.example.nhom03_organicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_info);
+    }
+}
