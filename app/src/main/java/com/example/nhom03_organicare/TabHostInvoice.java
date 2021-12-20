5f921596772f60ad8ef77c1472f58efd143ebf6e
Ánh Phan Hồng <anhph19411@st.uel.edu.vn>
2021-12-20 13:35:27 +0700

    change cursor color, fix tracking order

--- app/src/main/java/com/example/nhom03_organicare/TabHostInvoice.java
+++ app/src/main/java/com/example/nhom03_organicare/TabHostInvoice.java
@@ -2,8 +2,10 @@ package com.example.nhom03_organicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RatingBar;
