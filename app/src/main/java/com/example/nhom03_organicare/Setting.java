c7228a6e8cd7b90e97aded826c6cef5913651eef
Mike7501 <nhatpm19411@st.uel.edu.vn>
2021-12-24 17:16:50 +0700

    Dialog Log out

--- app/src/main/java/com/example/nhom03_organicare/Setting.java
+++ app/src/main/java/com/example/nhom03_organicare/Setting.java
@@ -1,7 +1,9 @@
 package com.example.nhom03_organicare;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class Setting extends AppCompatActivity {
-    LinearLayout layoutChangePassword, layoutFAQ;
+    LinearLayout layoutChangePassword, layoutFAQ,layoutLogout;
     ImageButton imbBack;
 
     @Override
@@ -24,6 +26,7 @@ public class Setting extends AppCompatActivity {
     private void linkViews() {
         layoutChangePassword = findViewById(R.id.layoutChangePassword);
         layoutFAQ = findViewById(R.id.layoutFAQ);
+        layoutLogout = findViewById(R.id.layoutLogout);
         imbBack = findViewById(R.id.imbBack);
     }
 
@@ -49,5 +52,33 @@ public class Setting extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        layoutLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XacNhanLogOut();
+            }
+        });
     }
+
+    private void XacNhanLogOut() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Thông báo !");
+        alertBuilder.setMessage("Bạn có muốn đăng xuất ?");
+
+        alertBuilder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Setting.this, AccountFragment.class );
+                startActivity(intent);
+            }
+        });
+        alertBuilder.setNegativeButton("Huỷ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertBuilder.show();
+    }
+
 }
